bf49301e14fc9f43b9b5a370db357dbfea7ac30d
Polly-coder <ushakovapa@gmail.com>
2022-02-07 17:53:57 +0300

    Initial HTML and JS

---
+++ cart.js
@@ -0,0 +1,40 @@
+const orderItems = [
+  {
+    name: 'Tea',
+    price: 1.99
+  },
+  {
+      name: 'Starbucks Coffee',
+      price: 8
+  },
+  {
+      name: 'Apple',
+      price: 2
+  },
+    {
+      name: 'Cola',
+      price: 1.5
+  },
+    {
+      name: 'Sweets',
+      price: 4.99
+  },
+    {
+      name: 'Soseges',
+      price: 2.8
+  },
+    {
+      name: 'Pie',
+      price: 5.99
+  },
+    {
+      name: 'Water',
+      price: 2
+  }
+];
+
+function getProducts() {
+    for(let i = 0; i<orderItems.length; i++){
+        let a = $('<div class="col col-xs-12 col-md-6 col-lg-4"></div>');
+    }
+}
